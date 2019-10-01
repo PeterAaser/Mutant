7c0f67ff33673bf8192a746308cc8127c24993a6
peteraa <peteraaser@gmail.com>
2019-10-01 16:06:08 +0200

    Fix diffusion and cytoplasma transfer

--- src/main/scala/fungus.scala
+++ src/main/scala/fungus.scala
@@ -4,6 +4,7 @@ import utils._
 import IntBonusOps._
 import cats._
 import cats.implicits._
+import collection.mutable.ArrayBuffer
 
 object Fungus {
 
@@ -49,24 +50,40 @@ object Fungus {
     def SUCC      = cell(0)
 
     // micro (anything)
+
+    // utility
+    def clear: Unit = for(ii <- 0 until cell.size){cell(ii) = 0.0}
   }
 
 
 
-  class mold(cells: Array[Array[Option[Cell]]]) {
+  abstract class Mold {
 
     // this should be baked into diffusionRateLookup
-    lazy val diffusionFactor = hardcode(1.0)
-    lazy val osmosisTransfer = hardcode(1.0)
+    lazy val diffusionFactor = hardcode(0.5)
+    lazy val osmosisTransfer = hardcode(0.2)
 
-    val diffusionRateLookup: Array[Double] = ???
+    val diffusionRates: Array[Double] = List.fill(10)(diffusionFactor).toArray
 
+    var liveCells: ArrayBuffer[(Int, Int)]
+
+    var cells:  Array[Array[Option[Cell]]]
+    var cellsB: Array[Array[Option[Cell]]]
 
     // For efficiency we use a recipient cell rather than folding a bunch of cells
     def transferSingle(center: Cell, neighbor: Cell, recipient: Cell): Unit = {
-      val pressureDiff = ((neighbor.cytoplasm + center.SUCC) - (center.cytoplasm + neighbor.SUCC))*osmosisTransfer
-      for(ii <- 1 until center.size){
-        recipient(ii) += neighbor(ii)*pressureDiff
+      val cytoTransfer = ((neighbor.cytoplasm + center.SUCC) - (center.cytoplasm + neighbor.SUCC))*osmosisTransfer
+
+      // Move cytoplasm
+      recipient(2) += cytoTransfer
+
+      // Recalculate concentrations
+      if(cytoTransfer > 0.0){
+        val oldCyto = center.cytoplasm/(center.cytoplasm + neighbor.cytoplasm)
+        val newCyto = 1.0 - oldCyto
+        for(ii <- 3 until center.size){
+          recipient(ii) = recipient(ii)*oldCyto + neighbor(ii)*newCyto
+        }
       }
     }
 
@@ -74,15 +91,21 @@ object Fungus {
     // Transfer based on osmosis
     def transfer(ii: Int, jj: Int, recipient: Cell): Unit = {
       val cell = cells(ii)(jj).get
-      cells.foreachNeighbours(ii)(jj)(transferSingle(cell,_,recipient))
+      cells.foreachNeighbours(ii)(jj)(transferSingle(cell,_,recipient)) 
     }
 
 
     // Transfer based on diffusion. Does not transfer energy or cytoplasm, doesn't care about SUCC
     def diffuseSingle(center: Cell, neighbor: Cell, recipient: Cell): Unit = {
-      for(ii <- 2 until center.size){
+
+      val centerCyto   = center.cytoplasm/(center.cytoplasm + neighbor.cytoplasm)
+      val neighborCyto = 1.0 - centerCyto
+
+      for(ii <- 3 until center.size){
         // Diffusionrate and factor can (and will) be prebaked
-        recipient(ii) += (neighbor(ii) - center(ii))*diffusionFactor*diffusionRateLookup(ii)
+        val loss = center(ii)*neighborCyto*diffusionRates(ii) 
+        val gain = neighbor(ii)*centerCyto*diffusionRates(ii)
+        recipient(ii) = center(ii) + gain - loss
       }
     }
 
@@ -90,6 +113,58 @@ object Fungus {
     def diffuse(ii: Int, jj: Int, recipient: Cell): Unit = {
       val cell = cells(ii)(jj).get
       cells.foreachNeighbours(ii)(jj)(diffuseSingle(cell,_,recipient))
+    }
+
+
+    // This is order dependent, I don't think that's a big deal
+    def stepOsmosis: Unit = liveCells.foreach{ case(ii, jj) =>
+      val recipient = cellsB(ii)(jj).get
+      for(kk <- 0 until recipient.size){ recipient(kk) = cells(ii)(jj).get(kk) }
+      transfer(ii, jj, recipient)
+      diffuse(ii, jj, recipient)
+    }
+
+
+    def stepFull: Unit = {
+      stepOsmosis
+      var tmp = cells
+      cells   = cellsB
+      cellsB  = tmp
+    }
+
+    def populateLiveCells: Unit =
+      for(ii <- 0 until cells.size){
+        for(jj <- 0 until cells.size){
+          if(cells(ii)(jj).isDefined) liveCells.append((ii, jj))
+        }
+      }
+  }
+  object Mold {
+
+    def apply(dim: Int): Mold = {
+      val ded: Option[Cell] = None
+      val m = new Mold {
+        override var cells     = List.fill(dim)(List.fill(dim)(ded).toArray).toArray
+        override var cellsB    = List.fill(dim)(List.fill(dim)(ded).toArray).toArray
+        override var liveCells = new ArrayBuffer[(Int, Int)]()
+      }
+
+      //                          SUCC, energy, cyto, signals
+      m.cells(2)(5) =  Some(Array(0.0,  1.0,    1.0,  2.0, 1.0, 1.0))
+      m.cells(3)(5) =  Some(Array(0.0,  1.0,    1.0,  0.0, 1.0, 1.0))
+      m.cells(4)(5) =  Some(Array(0.0,  1.0,    1.0,  0.0, 1.0, 1.0))
+      m.cells(5)(5) =  Some(Array(0.0,  1.0,    1.0,  0.0, 1.0, 1.0))
+      m.cells(6)(5) =  Some(Array(0.0,  1.0,    1.0,  0.0, 1.0, 1.0))
+      m.cells(7)(5) =  Some(Array(0.0,  1.0,    1.0,  5.0, 1.0, 1.0))
+      m.cellsB(2)(5) = Some(Array(0.0,  1.0,    0.0,  0.0, 1.0, 1.0))
+      m.cellsB(3)(5) = Some(Array(0.0,  1.0,    0.0,  0.0, 1.0, 1.0))
+      m.cellsB(4)(5) = Some(Array(0.0,  1.0,    0.0,  0.0, 1.0, 1.0))
+      m.cellsB(5)(5) = Some(Array(0.0,  1.0,    0.0,  0.0, 1.0, 1.0))
+      m.cellsB(6)(5) = Some(Array(0.0,  1.0,    0.0,  0.0, 1.0, 1.0))
+      m.cellsB(7)(5) = Some(Array(0.0,  1.0,    0.5,  0.0, 1.0, 1.0))
+
+      m.populateLiveCells
+      m
     }
   }
 }
--- src/main/scala/scalapagos.scala
+++ src/main/scala/scalapagos.scala
@@ -1,17 +1,21 @@
 package scalapagos
 
 import utils._
+import PrintUtils._
 
 object main {
   def main(args: Array[String]): Unit = {
 
-    ShittyTest.test
+    // ShittyTest.test
 
-    val hurr = (0 to 10).toArray
-    say(hurr.slice(0, 3).toList)
-    say(hurr.slice(3, 6).toList)
-    say(hurr.slice(6, 9).toList)
-    say(hurr.slice(9, 12).toList)
-    say(hurr.takeRight(3).toList)
+    val dur = Fungus.Mold(30)
+    say("\n\n\n\n\n\n\n\n\n\n")
+    say("\n\n\n\n\n\n\n\n\n\n")
+    say("\n\n\n\n\n\n\n\n\n\n")
+    for(ii <- 0 until 10){
+      say(printSlimeMoldSignals(dur.cells))
+      dur.stepFull
+    }
+       
   }
 }
